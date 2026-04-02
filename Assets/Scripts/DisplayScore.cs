d5178fa4868c0c0de18dcf80716ac57d8e5ac743
Bjørn Sødal <bjornsodal@gmail.com>
2026-04-02 13:16:03 +0200

    Jobber med oppsett av final scene for å hente ut score

---
+++ Assets/Scripts/DisplayScore.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DisplayScore : MonoBehaviour
+{
+    public int score;
+    void Start()
+    {
+    	// Fetch Score from MQTT and assign score value   
+    }
+    
+    public void display()
+    {
+    	// Function to be called once text loads
+    }
+
+}
