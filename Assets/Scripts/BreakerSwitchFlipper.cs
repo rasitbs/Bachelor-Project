7c1d68df619330777afc47c083ae39f517eceb52
Bjørn <bjornso@uia.no>
2026-04-06 10:40:16 +0200

    Kompilere scripts

--- Assets/Scripts/BreakerSwitchFlipper.cs
+++ Assets/Scripts/BreakerSwitchFlipper.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BrakerSwitchFlipper' : MonoBehaviour
+public class BreakerSwitchFlipper : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
