bfd47fe3e25d3dbce754a3a5749d21031ed07bee
Bjørn <bjornso@uia.no>
2026-04-09 12:58:37 +0200

    Changing plug code

--- Assets/Scripts/RedPlug.cs
+++ Assets/Scripts/RedPlug.cs
@@ -7,15 +7,5 @@ public class RedPlug : MonoBehaviour
     public bool isBlackFromConnection = false;
     public bool isBlackToConnection = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void 
 }
