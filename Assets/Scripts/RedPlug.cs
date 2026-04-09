a5a2d600c9d83bbd6da6d5f87fab044b3350bcaf
Bjørn <bjornso@uia.no>
2026-04-09 12:23:43 +0200

    Slette marius's bidrag, forlatt bachelor

--- Assets/Scripts/RedPlug.cs
+++ Assets/Scripts/RedPlug.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class RedPlug : MonoBehaviour
 {
+    public bool isRedFromConnection = false;
+    public bool isRedToConnection = false;
+    public bool isBlackFromConnection = false;
+    public bool isBlackToConnection = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
