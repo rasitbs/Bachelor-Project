a55852af10a80a508eb40896ebca529b011af235
Bjørn <bjornso@uia.no>
2026-04-09 14:55:00 +0200

    Fuck Gemini, kanke drit :(

--- Assets/Scripts/MultimeterScreenUpdater.cs
+++ Assets/Scripts/MultimeterScreenUpdater.cs
@@ -3,29 +3,5 @@ using TMPro;
 
 public class MultimeterScreenUpdater : MonoBehaviour
 {
-    [SerializeField] private MultimeterProbe redProbe;
-    [SerializeField] private MultimeterProbe blackProbe;
-
-    public TextMeshProUGUI voltageText;
-    public bool isMeasuringVoltage = true;
-
-    void Update()
-    {
-        if (!isMeasuringVoltage) return;
-
-        // Both probes must be plugged in to complete a circuit
-        if (redProbe.IsConnected && blackProbe.IsConnected)
-        {
-            // Calculate voltage difference: Red Probe Voltage - Black Probe Voltage
-            float measuredVoltage = redProbe.GetPotential() - blackProbe.GetPotential();
-
-            // Format to show a whole number (e.g., 230V or -230V)
-            voltageText.text = Mathf.RoundToInt(measuredVoltage).ToString() + "V";
-        }
-        else
-        {
-            // If one or both are unplugged, show 0V or a disconnected state
-            voltageText.text = "0V";
-        }
-    }
+    
 }
