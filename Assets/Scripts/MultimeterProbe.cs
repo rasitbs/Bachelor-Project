99cecc37fc83a701a865fe4d6f62ae059151a858
Bjørn <bjornso@uia.no>
2026-04-10 10:37:46 +0200

    Prøve å fikse multimeter scripts

--- Assets/Scripts/MultimeterProbe.cs
+++ Assets/Scripts/MultimeterProbe.cs
@@ -2,5 +2,36 @@ using UnityEngine;
 
 public class MultimeterProbe : MonoBehaviour
 {
-   
+    public bool isRedTo = false;
+    public bool isRedFrom = false;
+    public bool isBlackTo = false;
+    public bool isBlackFrom = false;
+
+    public void Connect(GameObject other)
+    {
+        if(other.CompareTag("Hot Point Black From"))
+        {
+            isBlackFrom = true;
+        }
+        else if(other.CompareTag("Hot Point Black To"))
+        {
+            isBlackTo = true;
+        }
+        else if(other.CompareTag("Hot Point Red From"))
+        {
+            isRedFrom = true;
+        }
+        else if(other.CompareTag("Hot Point Red To"))
+        {
+            isRedTo = true;
+        }
+    }
+
+    public void Disconnect()
+    {
+        isRedTo = false;
+        isRedFrom = false;
+        isBlackTo = false;
+        isBlackFrom = false;
+    }
 }
--- Assets/Scripts/MultimeterScreenUpdater.cs
+++ Assets/Scripts/MultimeterScreenUpdater.cs
@@ -1,7 +1,31 @@
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class MultimeterScreenUpdater : MonoBehaviour
 {
+    private MultimeterProbe redprobe;
+    private MultimeterProbe blackprobe;
+    private BreakerSwitchFlipper breakerswitch;
+    private TMPro.TextMeshProUGUI voltageText;
     
+    void Start()
+    {
+        redprobe = GameObject.Find("RedWirePlug").GetComponent<MultimeterProbe>();
+        blackprobe = GameObject.Find("BlackWirePlug").GetComponent<MultimeterProbe>();
+        voltageText = GameObject.Find("MultimeterScreen").GetComponent<TMPro.TextMeshProUGUI>();
+        breakerswitch = GameObject.Find("Breaker Switch").GetComponent<BreakerSwitchFlipper>();
+    }
+
+    void Update()
+    {
+        if((redprobe.isRedTo && blackprobe.isBlackTo) || (breakerswitch.isFlipped && redprobe.isRedFrom && blackprobe.isBlackFrom))
+        {
+            voltageText.text = "230V";
+        }
+        else
+        {
+            voltageText.text = "---V";
+        }
+    }
 }
