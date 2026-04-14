f748bb49ef63c853577fa5010dde110a21d598a9
Bjørn <bjornso@uia.no>
2026-04-14 13:38:10 +0200

    Repositioning

--- Assets/Scripts/Skrue.cs
+++ Assets/Scripts/Skrue.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Skrue : MonoBehaviour
 {
     private bool skrudd = true;
+    private Vector3 skrudd_pos = new Vector3(0.458324879f, 0.0172838494f, -0.657422066f);
+    private Vector3 uskrudd_pos = new Vector3(0.458324879f, -0.024f, -0.657422066f);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
